fb590deae7154cc6554896c8ad6337113eefe15f
Juan Francisco Rivera Hernandez <jfrivera.premier@gmail.com>
2025-01-05 13:25:48 -0300

    creacion de clase ConsumoAPI y cambio de rama a main

--- src/main/java/com/aluracursos/Literalura/LiteraluraApplication.java
+++ src/main/java/com/aluracursos/Literalura/LiteraluraApplication.java
@@ -1,5 +1,6 @@
 package com.aluracursos.Literalura;
 
+import com.aluracursos.Literalura.Service.ConsumoAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LiteraluraApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LiteraluraApplication.class, args);
+
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obtenerDatos("https://gutendex.com/books/");
+		//System.out.println(json);
 	}
 
 }
